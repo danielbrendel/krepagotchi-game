5331f0fe34df7ff109d846445f5bd8becf4aa945
Daniel Brendel <dbrendel1988@gmail.com>
2025-07-20 17:50:55 +0200

    Fix default value for release command

--- app/commands/CmdGameBuildRelease.php
+++ app/commands/CmdGameBuildRelease.php
@@ -3,7 +3,7 @@
 class CmdGameBuildRelease implements Asatru\Commands\Command  {
     public function handle($args)
     {
-        $bundle = $args?->get(0)?->getValue(1);
+        $bundle = $args?->get(0)?->getValue(1) ?? 'html5';
 
         GameBuild::make($bundle, false);
     }
