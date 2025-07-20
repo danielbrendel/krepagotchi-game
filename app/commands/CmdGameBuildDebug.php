c3f25a0f16608513d75d6b6f06529ceb1ed611b5
Daniel Brendel <dbrendel1988@gmail.com>
2025-07-20 17:52:23 +0200

    Fix default value for debug command

--- app/commands/CmdGameBuildDebug.php
+++ app/commands/CmdGameBuildDebug.php
@@ -3,7 +3,7 @@
 class CmdGameBuildDebug implements Asatru\Commands\Command  {
     public function handle($args)
     {
-        $bundle = $args?->get(0)?->getValue(1);
+        $bundle = $args?->get(0)?->getValue(1) ?? 'html5';
 
         GameBuild::make($bundle, true);
     }
