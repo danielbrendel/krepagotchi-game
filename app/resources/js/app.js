add9874a8d3070f88fb2b5558fb29d16e789249e
Daniel Brendel <dbrendel1988@gmail.com>
2025-07-20 20:55:35 +0200

    Wording

--- app/resources/js/app.js
+++ app/resources/js/app.js
@@ -288,7 +288,7 @@ window.openArchive = function(title = 'Letter Archive') {
                 }
             }
             } else {
-                elList.innerHTML = `<i>You don't have any letters saved.</i>`
+                elList.innerHTML = `<i>You don't have any saved letters.</i>`
             }
         }
 
